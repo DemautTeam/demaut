7c5fe2dadb53e508192b7435d230af59cfea4f13
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-08-31 09:37:57 +0200

    Première intégration du front angular, basé sur les travaux du backoffice

--- demaut-project/demaut-cyber/src/main/java/ch/vd/ses/demaut/portail/appliquette/BootStrap.java
+++ demaut-project/demaut-cyber/src/main/java/ch/vd/ses/demaut/portail/appliquette/BootStrap.java
@@ -9,7 +9,9 @@ import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
@@ -21,14 +23,18 @@ import java.util.List;
 @EnableAutoConfiguration
 @ComponentScan
 @ImportResource({"classpath:META-INF/cxf/cxf.xml"})
-@PropertySource("classpath:")
-public class BootStrap {
+public class BootStrap extends SpringBootServletInitializer {
 
     @Autowired
     private ApplicationContext applicationContext;
 
-    public static void main(String... args) {
+    /*public static void main(String... args) {
         SpringApplication.run(BootStrap.class, args);
+    }*/
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootStrap.class);
     }
 
     @Bean
