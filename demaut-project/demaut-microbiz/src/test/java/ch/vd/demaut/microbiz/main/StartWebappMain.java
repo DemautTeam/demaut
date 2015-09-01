83c9edfe16003c69b279deb215fc25adf3aeb4dc
b5cav2 <ezzedine.thebti@vd.ch>
2015-09-01 12:47:23 +0200

    ajout doc

--- demaut-project/demaut-microbiz/src/test/java/ch/vd/demaut/microbiz/main/StartWebappMain.java
+++ demaut-project/demaut-microbiz/src/test/java/ch/vd/demaut/microbiz/main/StartWebappMain.java
@@ -4,10 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class StartWebappMain {
 
-    private static ClassPathXmlApplicationContext xmlApplicationContext;
-
-	public static void main(String[] args) throws Exception {
-        xmlApplicationContext = new ClassPathXmlApplicationContext("/microbizTest-context.xml");
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext xmlApplicationContext = new ClassPathXmlApplicationContext("/microbizTest-context.xml");
         xmlApplicationContext.registerShutdownHook();
     }
 }
