d066d91899763ba89dadaf21c3b41de31f029d2d
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-09-09 15:30:27 +0200

    Création de la directive pour les boutons de validation

--- demaut-project/demaut-commons-web/src/main/resources/public/js/commons-module.js
+++ demaut-project/demaut-commons-web/src/main/resources/public/js/commons-module.js
@@ -6,3 +6,15 @@ commonsModule.service('nationalityTest', ['$log', function ($log) {
         return this.suissePattern.test(textValue);
     };
 }]);
+
+commonsModule.directive('stepActions', function(){
+    return {
+        restrict: 'E',
+        scope:{previewStepFn:'&',nextStepFn:'&', cancelAction:'&'},
+        templateUrl: 'template/directive/step-actions.html',
+        link: function (scope,element, attrs) {
+            scope.showNext = element.attr('next-step-fn');
+            scope.showPreview = element.attr('preview-step-fn');
+        }
+    };
+});
