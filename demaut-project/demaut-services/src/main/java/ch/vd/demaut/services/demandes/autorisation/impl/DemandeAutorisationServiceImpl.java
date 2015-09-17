f0d1870a3cc3572d2c5734643f61ed7a86bcb676
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-09-17 11:54:16 +0200

    Merge branch 'cmj'
    # Conflicts:
    #	demaut-project/demaut-services/src/main/java/ch/vd/demaut/services/demandes/autorisation/impl/DemandeAutorisationServiceImpl.java
    
    
    Former-commit-id: 3876157ec2ce08e6769081c22d07790ae7be35bc

--- demaut-project/demaut-services/src/main/java/ch/vd/demaut/services/demandes/autorisation/impl/DemandeAutorisationServiceImpl.java
+++ demaut-project/demaut-services/src/main/java/ch/vd/demaut/services/demandes/autorisation/impl/DemandeAutorisationServiceImpl.java
@@ -1,22 +1,7 @@
 package ch.vd.demaut.services.demandes.autorisation.impl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Collection;
-
-import javax.inject.Inject;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.transaction.annotation.Transactional;
-
-import ch.vd.demaut.domain.annexes.Annexe;
-import ch.vd.demaut.domain.annexes.AnnexeMetadata;
 import ch.vd.demaut.domain.annexes.AnnexeNonValideException;
 import ch.vd.demaut.domain.annexes.ContenuAnnexe;
-import ch.vd.demaut.domain.annexes.NomFichier;
-import ch.vd.demaut.domain.annexes.TypeAnnexe;
 import ch.vd.demaut.domain.demandes.ReferenceDeDemande;
 import ch.vd.demaut.domain.demandes.autorisation.DemandeAutorisation;
 import ch.vd.demaut.domain.demandes.autorisation.DemandeAutorisationFactory;
@@ -24,29 +9,44 @@ import ch.vd.demaut.domain.demandes.autorisation.ProfessionDeLaSante;
 import ch.vd.demaut.domain.demandes.autorisation.repo.DemandeAutorisationRepository;
 import ch.vd.demaut.domain.utilisateurs.Login;
 import ch.vd.demaut.services.demandes.autorisation.DemandeAutorisationService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class DemandeAutorisationServiceImpl implements DemandeAutorisationService {
 
-	@Inject
-	private DemandeAutorisationRepository demandeAutorisationRepository;
-	
-	@Inject
-	private DemandeAutorisationFactory demandeAutorisationFactory;
+    @Inject
+    private DemandeAutorisationRepository demandeAutorisationRepository;
 
-	@Transactional
-	@Override
-	public DemandeAutorisation initialiserDemandeAutorisation(Login login, ProfessionDeLaSante profession) {
-		DemandeAutorisation nouvelleDemande = demandeAutorisationFactory.inititierDemandeAutorisation(login, profession, null);
-		
-		demandeAutorisationRepository.store(nouvelleDemande);
-				
-		return nouvelleDemande;
-	}
-	
-	@Transactional(readOnly=true)
-	@Override
-	public DemandeAutorisation recupererDemandeParReference(ReferenceDeDemande ref) {
-		return demandeAutorisationRepository.recupererDemandeParReference(ref);
-	}
+    @Inject
+    private DemandeAutorisationFactory demandeAutorisationFactory;
+
+    @Transactional
+    @Override
+    public DemandeAutorisation initialiserDemandeAutorisation(Login login, ProfessionDeLaSante professionDeLaSante) {
+        DemandeAutorisation demandeAutorisation = demandeAutorisationFactory.initierDemandeAutorisation(login, professionDeLaSante, null);
+        demandeAutorisationRepository.store(demandeAutorisation);
+        return demandeAutorisation;
+    }
+
+    @Override
+    public DemandeAutorisation recupererDemandeParReference(ReferenceDeDemande referenceDeDemande) {
+        return demandeAutorisationRepository.recupererDemandeParReference(referenceDeDemande);
+    }
+
+    @SuppressWarnings("unchecked")
+    private ContenuAnnexe buildContenuAnnexe(File file) {
+        byte[] contenu;
+        try {
+            contenu = IOUtils.toByteArray(new FileInputStream(file));
+        } catch (IOException e) {
+            throw new AnnexeNonValideException();
+        }
+        return new ContenuAnnexe(contenu);
+    }
 
 }
