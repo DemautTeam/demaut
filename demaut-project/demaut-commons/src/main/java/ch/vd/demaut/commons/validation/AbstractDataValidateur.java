01b84c67caa6b74eb317ef1274cf77a67e61c28c
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-09-24 16:43:25 +0200

    evolution des validateurs
    Former-commit-id: a49892993ebf3f5cfa40871c1cfd6f16adbfe343

---
+++ demaut-project/demaut-commons/src/main/java/ch/vd/demaut/commons/validation/AbstractDataValidateur.java
@@ -0,0 +1,26 @@
+package ch.vd.demaut.commons.validation;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+/**
+ * Initialise un validator
+ */
+public abstract class AbstractDataValidateur<T> implements DataValidateur<T> {
+
+    private final Validator validator;
+
+    protected AbstractDataValidateur(){
+        validator = ValidatorFactoryDefault.getValidator();
+    }
+
+    protected Validator getValidator(){
+        return validator;
+    }
+
+    protected Set<ConstraintViolation<T>> validateData(T data){
+        return validator.validate(data);
+    }
+
+}
