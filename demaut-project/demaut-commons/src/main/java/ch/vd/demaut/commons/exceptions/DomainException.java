9e821698aba28cf1f66eaae0c2a5f2a50aab07e7
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-09-24 16:06:05 +0200

    ajout des constructeurs
    Former-commit-id: 7bfb5f1e744cddad031ee964e109e4b7c67d4842

--- demaut-project/demaut-commons/src/main/java/ch/vd/demaut/commons/exceptions/DomainException.java
+++ demaut-project/demaut-commons/src/main/java/ch/vd/demaut/commons/exceptions/DomainException.java
@@ -8,8 +8,59 @@ public class DomainException extends BaseException {
 
     private static final long serialVersionUID = 6413485124943479155L;
 
-    protected DomainException() {
-        super();
+
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public DomainException() {
     }
 
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public DomainException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified cause and a
+     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
+     * (which typically contains the class and detail message of
+     * <tt>cause</tt>).  This constructor is useful for runtime exceptions
+     * that are little more than wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A <tt>null</tt> value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     * @since 1.4
+     */
+    public DomainException(Throwable cause) {
+        super(cause);
+    }
 }
