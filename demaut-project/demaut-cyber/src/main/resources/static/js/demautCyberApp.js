0d92b74a421d63957fa3137011b893eba6d93b6e
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2015-10-01 17:47:45 +0200

    Qques changements divers pour la MeIN
    Former-commit-id: f2ca0cef5094a265d22016b13698efc530b274f9

--- demaut-project/demaut-cyber/src/main/resources/static/js/demautCyberApp.js
+++ demaut-project/demaut-cyber/src/main/resources/static/js/demautCyberApp.js
@@ -1,10 +1,10 @@
-var ngDemautCyber = angular.module('ngDemautCyber', ['ngDemautApp']);
+var ngDemautCyber = angular.module('ngDemautCyber', ['ngDemautApp', 'portail']);
 /**
  * l'injection du service $route dans la directive est un contournement d'un bug de ngRoute
  */
 ngDemautCyber.directive('demautContainer', ['$route', function ($route) {
     return {
         restrict: 'E',
-        templateUrl: 'html/demaut.html'
+        templateUrl: prestationContext+'/html/demaut.html'
     };
 }]);
