c4282063a3f44c18c57cc612eb4c2450a55ba34b
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-10-05 11:10:30 +0200

    mise à jour du script de deploiement Cyber
    Former-commit-id: 0bf61102c7168ae4f2aabaf78f58b7efb055caa3

--- config/jenkins/deployDemautCyber.sh
+++ config/jenkins/deployDemautCyber.sh
@@ -22,23 +22,24 @@ projectConfigName=application
 tomcatConfigName=demaut-cyber.xml
 projectBasedir=${WORKSPACE}/demaut-project/$projectFolderName
 
-# deploy to smx4
+# deploy to demo
 component=$projectFolderName
 pathServer=/ccv/data/dsi_cyber/demautIN
 remoteBin=$pathServer
-remoteDeploy=$pathServer/app/demaut/deployment
-remoteConfig=$pathServer/app/demaut/config
+remoteBaseApp=$pathServer/app/demaut
+#remoteDeploy=$pathServer/app/demaut/deployment
+#remoteConfig=$pathServer/app/demaut/config
 remoteTomcatConfig=$pathServer/conf/Catalina/localhost
 remoteServer=dsi_cyber@slv2395t.etat-de-vaud.ch
 #contourne l'alerte de sécurité de SSH
 chmod 600 ${WORKSPACE}/config/jenkins/id.rsa.jenkins
 sshOptions="-o StrictHostKeyChecking=no -i ${WORKSPACE}/config/jenkins/id.rsa.jenkins"
 
-echo Rechercher bundle à déployer $component : `ls $projectBasedir/target/$component-*.war`
+echo Rechercher bundle à déployer $component : `ls $projectBasedir/target/$component-*.tar.gz`
 
-if [ -f $projectBasedir/target/$component-*.war ]
+if [ -f $projectBasedir/target/$component-*.tar.gz ]
 then
-	 echo Nouveau bundle à déployer: `ls $projectBasedir/target/$component-*.war`
+	 echo Nouveau bundle à déployer: `ls $projectBasedir/target/$component-*.tar.gz`
 else
      echo "Pas de bundle à déployer dans target. Veuillez compiler le projet"
     exit 0
@@ -50,36 +51,31 @@ echo "Stop du container Tomcat..."
 ssh $sshOptions $remoteServer $remoteBin/tomcatctl.sh stop
 echo "Stop du container Tomcat terminé"
 
-
 echo "waiting 5s....."
 sleep 5
-echo "Mise à jour du fichier de configuration sur $remoteServer:$remoteConfig..."
-scp $sshOptions "$projectBasedir/conf/$projectConfigName.properties" $remoteServer:$remoteConfig
-echo "Mise à jour du fichier de configuration terminée"
 
-echo "waiting 5s....."
-sleep 5
+echo "Cleaning Tomcat's container..."
+ssh $sshOptions $remoteServer $remoteBin/tomcatctl.sh clean
+echo "Cleaning Tomcat's container, done..."
+
+echo "Suppression de l'ancien bundle..."
+ssh $sshOptions $remoteServer rm -rf $remoteBaseApp/$component-*
+echo "Copie de la nouvelle version..."
+scp $sshOptions "$projectBasedir/target/$component-*.tar.gz" $remoteServer:$remoteBaseApp
+echo "Décompression de la nouvelle version..."
+ssh $sshOptions $remoteServer tar -xzvf $remoteBaseApp/$component-*.tar.gz
+echo "Mise à jour du bundle terminée"
+
 echo "Mise à jour du fichier de configuration tomcat sur $remoteServer:$remoteTomcatConfig..."
 scp $sshOptions "$projectBasedir/tomcat/$tomcatConfigName.xml" $remoteServer:$remoteTomcatConfig
 echo "Mise à jour du fichier de configuration tomcat terminée"
 
-echo "waiting 5s....."
-
-echo "Suppression de l'ancien bundle sur le serveur $remoteServer:$remoteDeploy"
-echo "Emplacement du fichier sur le serveur:"
-ssh $sshOptions $remoteServer ls $remoteDeploy/$component-*.war
-echo "Suppression..."
-ssh $sshOptions $remoteServer rm $remoteDeploy/$component-*.war
-echo "waiting 5s....."
-sleep 5
-echo "deploying new bundle  in $remoteServer:$remoteDeploy"
-ls $projectBasedir/target/$component-*.war
-scp $sshOptions $projectBasedir/target/$component-*.war $remoteServer:$remoteDeploy/$component.war
-echo "waiting 5s....."
-sleep 5
-echo "Start du container MicroBiz..."
+echo "Start du tomcat cyber..."
 ssh $sshOptions $remoteServer $remoteBin/tomcatctl.sh start
-echo "Start du container MicroBiz terminé"
-echo "Status du container MicroBiz..."
+echo "Start du tomcat cyber terminé"
+
+echo "waiting 5s....."
+sleep 5
+echo "Status du tomcat cyber..."
 ssh $sshOptions $remoteServer $remoteBin/tomcatctl.sh check
 
