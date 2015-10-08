2712fe5b088a0d5472ccedf28cd04a6d9992e0b2
Fabien Benichou <fabien.benichou@vd.ch>
2015-10-08 18:29:08 +0200

    - Remove warnings
    Former-commit-id: 24e8948295edd143b206f498f370fe9d4d370849

--- demaut-project/demaut-cyber/src/main/java/ch/vd/ses/demaut/portail/service/impl/ConfiguratorServiceImpl.java
+++ demaut-project/demaut-cyber/src/main/java/ch/vd/ses/demaut/portail/service/impl/ConfiguratorServiceImpl.java
@@ -1,20 +1,15 @@
 package ch.vd.ses.demaut.portail.service.impl;
 
-import ch.vd.ses.demaut.portail.service.ConfiguratorService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import ch.vd.ses.demaut.portail.service.ConfiguratorService;
 
 /**
  *
  */
 @Service("configuratorService")
 public class ConfiguratorServiceImpl implements ConfiguratorService{
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Value("${baseMicrobiz}")
     private String urlPrefix;
