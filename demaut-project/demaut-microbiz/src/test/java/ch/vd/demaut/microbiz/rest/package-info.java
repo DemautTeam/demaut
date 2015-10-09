2e9b907c888220ad30f69f936945eac1a6e2a5f7
Fabien Benichou <fabien.benichou@vd.ch>
2015-10-09 09:06:49 +0200

    Créer War again...
    Former-commit-id: e24036fc41e68cb1849a520e174abd7e93ececf1

---
+++ demaut-project/demaut-microbiz/src/test/java/ch/vd/demaut/microbiz/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 
+ */
+package ch.vd.demaut.microbiz.rest;
--- demaut-project/demaut-rest/src/test/java/ch/vd/demaut/microbiz/rest/AnnexeRestImplTest.java
+++ demaut-project/demaut-rest/src/test/java/ch/vd/demaut/microbiz/rest/AnnexeRestImplTest.java
@@ -1,5 +1,22 @@
 package ch.vd.demaut.microbiz.rest;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import ch.vd.demaut.domain.annexes.Annexe;
 import ch.vd.demaut.domain.annexes.TypeAnnexe;
 import ch.vd.demaut.domain.demandes.ReferenceDeDemande;
@@ -8,28 +25,8 @@ import ch.vd.demaut.domain.demandes.autorisation.Profession;
 import ch.vd.demaut.domain.utilisateurs.Login;
 import ch.vd.demaut.rest.services.impl.AnnexeRestImpl;
 import ch.vd.demaut.services.demandes.autorisation.DemandeAutorisationService;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.FileInputStream;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 @Ignore("Unable to instantiate Configuration ValidationException")
-@ContextConfiguration({"classpath*:microbizTest-context.xml"})
-@RunWith(SpringJUnit4ClassRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AnnexeRestImplTest {
 
