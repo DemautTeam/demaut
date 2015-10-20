532d8d6d2be3178733befb3ec2bf6e31dc50b326
Fabien Benichou <fabien.benichou@vd.ch>
2015-10-20 17:52:33 +0200

    few comments
    Former-commit-id: fc796903352aaf22d5a4db563057875ec0885499

--- demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/donneesProf/diplome/ReferenceDeDiplome.java
+++ demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/donneesProf/diplome/ReferenceDeDiplome.java
@@ -3,6 +3,7 @@ package ch.vd.demaut.domain.demandeur.donneesProf.diplome;
 import ch.vd.demaut.commons.annotations.ValueObject;
 import ch.vd.demaut.commons.vo.StringVO;
 
+//TODO: A virer absolument. Pas besoin de Reference De Diplome ! 
 @ValueObject
 public class ReferenceDeDiplome extends StringVO {
 
