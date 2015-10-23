1b885335ca97e4ecf78fdfb419e5ce35b0bc8905
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-10-23 16:41:37 +0200

    ajout d'une methode utilitaire
    Former-commit-id: 948289956f38871d0004ec8cf3e8b94a9b67049f

--- demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/DonneesPersonnellesSteps.java
+++ demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/DonneesPersonnellesSteps.java
@@ -2,6 +2,7 @@ package ch.vd.demaut.cucumber.steps;
 
 import ch.vd.demaut.commons.bdd.AccepteOuRefuse;
 import ch.vd.demaut.commons.exceptions.DomainException;
+import ch.vd.demaut.domain.demandeur.Localite;
 import ch.vd.demaut.domain.demandeur.Pays;
 import ch.vd.demaut.domain.demandeur.donneesPerso.*;
 import org.joda.time.LocalDate;
