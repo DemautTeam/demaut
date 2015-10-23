21b22f7738be1a9aefd26a6bb15fb835d405c0ae
Charles Mourad Jaber <charles-mourad.jaber@vd.ch>
2015-10-23 17:29:40 +0200

    refactoring
    Former-commit-id: 132b8947cc10b7b1e3e5a48f3e20524b5a885c5a

--- demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/Fax.java
+++ demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/Fax.java
@@ -1,9 +1,11 @@
 package ch.vd.demaut.domain.demandeur;
 
+import ch.vd.demaut.commons.annotations.ValueObject;
 import ch.vd.demaut.commons.vo.StringVO;
 
 import javax.validation.constraints.Pattern;
 
+@ValueObject
 public class Fax extends StringVO {
 
     public Fax() {
