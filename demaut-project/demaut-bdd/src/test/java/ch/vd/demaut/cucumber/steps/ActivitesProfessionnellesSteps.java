756c7c8c2811b56727a766a309a2f5756bc6028d
Aécio Miguel <aecio-miguel.dos-anjos@vd.ch>
2015-11-03 17:15:13 +0100

    Ajouté les definitions pour activité professionnelles $
    Former-commit-id: ca4251bed51ab921c6ec1ff642755c0fa591a024

--- demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/ActivitesProfessionnellesSteps.java
+++ demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/ActivitesProfessionnellesSteps.java
@@ -1,7 +1,43 @@
 package ch.vd.demaut.cucumber.steps;
 
-/**
- * Created by aecio on 03.11.15.
- */
+import ch.vd.demaut.commons.bdd.AccepteOuRefuse;
+import ch.vd.demaut.domain.demandeur.activiteProf.ActiviteProfessionnelleEnvisage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ActivitesProfessionnellesSteps {
+
+    // ********************************************************* Static fields
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    // ********************************************************* Fields
+
+    private ActiviteProfessionnelleEnvisage currentDonnees;
+
+    private AccepteOuRefuse actionActuelle;
+    private DemandeAutorisationSteps demandeAutorisationSteps;
+
+    // *********************************************** Technical Methods
+
+    public DemandeAutorisationSteps getDemandeAutorisationSteps() {
+        return demandeAutorisationSteps;
+    }
+
+    public void setDemandeAutorisationSteps(DemandeAutorisationSteps demandeAutorisationSteps) {
+        this.demandeAutorisationSteps = demandeAutorisationSteps;
+    }
+
+    public void initDemandeur() {
+
+
+    }
+
+
+
+
+    //************************************************************
+
+
+
 }
--- demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/definitions/ActivitesProfessionnellesDefinitions.java
+++ demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/definitions/ActivitesProfessionnellesDefinitions.java
@@ -1,15 +1,39 @@
 package ch.vd.demaut.cucumber.steps.definitions;
 
-import cucumber.api.PendingException;
+import ch.vd.demaut.cucumber.converteurs.commons.NPAConverter;
+import ch.vd.demaut.cucumber.steps.ActivitesProfessionnellesSteps;
+import ch.vd.demaut.domain.demandeur.donneesPerso.NPA;
+import cucumber.api.Transform;
 import cucumber.api.java.fr.Etantdonné;
 
 /**
- * Created by aecio on 03.11.15.
+ * Step definitions pour la "Saisie d'activités professionnelles"
  */
-public class ActivitesProfessionnellesDefinitions {
-    @Etantdonné("^un NPA prof. de format numérique et de (\\d+) caractères$")
-    public void un_NPA_prof_de_format_numérique_et_de_caractères(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+public class ActivitesProfessionnellesDefinitions extends StepDefinitions {
+
+
+    // ********************************************************* Fields
+
+    private ActivitesProfessionnellesSteps activitesProfessionnellesSteps;
+
+    // ********************************************************* Technical
+    // methods
+
+    public void setActivitesProfessionnellesSteps(ActivitesProfessionnellesSteps activitesProfessionnellesSteps) {
+        this.activitesProfessionnellesSteps = activitesProfessionnellesSteps;
     }
+
+    // ********************************************************* Given
+
+
+
+    @Etantdonné("^un NPA prof. de format numérique et de (\\d+) caractères$")
+    public void un_NPA_prof_de_format_numérique_et_de_caractères(@Transform(NPAConverter.class) NPA npa) throws Throwable {
+        activitesProfessionnellesSteps.initDemandeur();
+        //activitesProfessionnellesSteps.setNPA(npa);
+
+
+    }
+
+
 }
