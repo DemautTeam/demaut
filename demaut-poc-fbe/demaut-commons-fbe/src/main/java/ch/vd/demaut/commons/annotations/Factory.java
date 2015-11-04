b1fb0c42676b57f5b557428fad473778b29e4394
Fabien Benichou <fabien.benichou@vd.ch>
2015-11-04 19:51:00 +0100

    Supression des repertoires des Pocs
    Former-commit-id: b13f769d0f3283784385992824397a3ab6cbf8f0

--- demaut-poc-fbe/demaut-commons-fbe/src/main/java/ch/vd/demaut/commons/annotations/Factory.java
+++
@@ -1,9 +0,0 @@
-package ch.vd.demaut.commons.annotations;
-
-/**
- * Identifies a Factory in the DDD context. 
- *
- */
-public @interface Factory {
-
-}
--- demaut-poc-fbe/demaut-domain-fbe/src/main/java/ch/vd/demaut/commons/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains common classes used by all entities and VO of the OMgt domain model
- */
-package ch.vd.demaut.commons;
