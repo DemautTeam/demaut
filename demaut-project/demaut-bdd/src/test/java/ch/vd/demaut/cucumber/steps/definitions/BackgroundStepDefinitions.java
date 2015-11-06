1a38c429fdad0f1a2584942c223d287da9ce9455
Fabien Benichou <fabien.benichou@vd.ch>
2015-11-06 12:23:46 +0100

    MAJ des scenarios avec les professions et utilisation de la liste du Jira DEMAUT-36
    - Veirfie les labels, code GLN obligaotoire et Categorie
    - MAJ de la liste des professions
    
    
    Former-commit-id: b4a70f4d8906743d44e83a2f916db8890143da27

--- demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/definitions/BackgroundStepDefinitions.java
+++ demaut-project/demaut-bdd/src/test/java/ch/vd/demaut/cucumber/steps/definitions/BackgroundStepDefinitions.java
@@ -2,14 +2,19 @@ package ch.vd.demaut.cucumber.steps.definitions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.vd.demaut.cucumber.converteurs.commons.DateTimeConverter;
+import ch.vd.demaut.cucumber.converteurs.commons.OuiNonConverter;
 import ch.vd.demaut.cucumber.steps.BackgroundSteps;
+import ch.vd.demaut.domain.demandes.autorisation.CategorieProfession;
 import ch.vd.demaut.domain.demandes.autorisation.Profession;
 import cucumber.api.DataTable;
 import cucumber.api.Transform;
@@ -54,15 +59,14 @@ public class BackgroundStepDefinitions extends StepDefinitions {
         LOGGER.debug("La date du jour est: " + dateHeureCourant);
     }
     
-    @Etantdonné("^les professions nécessitant un code GLN obligatoire:$")
+    @Etantdonné("^les professions disponibles avec les caractéristiques suivantes:$")
     public void les_professions_nécessitant_un_code_GLN_obligatoire(DataTable dataTable) throws Throwable {
 
-        List<Profession> professionsAvecCodeGlnAttendues = dataTable.asList(Profession.class);
+        List<Profession> professionsAttendues = buildListeProfessionsAttenduesEtTesterCaracteristiques(dataTable);
 
-        //TODO: Tester dans BackgroundSteps
-        List<Profession> professionsAvecCodeGlnConfigurees = Profession.listerProfessionsAvecCodeGlnObligatoire();
-        assertThat(professionsAvecCodeGlnConfigurees).hasSameSizeAs(professionsAvecCodeGlnAttendues);
-        assertThat(professionsAvecCodeGlnConfigurees).containsAll(professionsAvecCodeGlnAttendues);
+        List<Profession> professionsConfigurees = Arrays.asList(Profession.values());
+        assertThat(professionsConfigurees).hasSameSizeAs(professionsAttendues);
+        assertThat(professionsConfigurees).containsAll(professionsAttendues);
     }
 
     // ********************************************************* Setters for Spring
@@ -71,4 +75,42 @@ public class BackgroundStepDefinitions extends StepDefinitions {
         this.backgroundSteps = backgroundSteps;
     }
 
+    // ********************************************************* Private methods
+    
+    private List<Profession> buildListeProfessionsAttenduesEtTesterCaracteristiques(DataTable dataTable) {
+
+        List<Profession> professionsAttendues = new ArrayList<Profession>();
+
+        List<Map<String, String>> professionsMap = dataTable.asMaps(String.class, String.class);
+        for (Map<String, String> professionMap : professionsMap) {
+            
+            //Recupère les caractéristiques de la profession attendue
+            String labelProfession = professionMap.get("Profession");
+            
+            Boolean codeGLNObligatoireAttendue = parseCodeGLNObligatoire(professionMap);
+            
+            CategorieProfession categorieAttendue = parseCategorieProfession(professionMap);
+            
+            //Verifie si caracteristiques de la profession sont ok
+            Profession professionAttendue = Profession.getByLibele(labelProfession);
+            assertThat(professionAttendue.isCodeGLNObligatoire()).isEqualTo(codeGLNObligatoireAttendue);
+            assertThat(professionAttendue.getCategorie()).isEqualTo(categorieAttendue);
+            
+            //Ajoute dans la liste des professions attendues
+            professionsAttendues.add(professionAttendue);
+        }
+
+        return professionsAttendues;
+    }
+
+    private Boolean parseCodeGLNObligatoire(Map<String, String> professionMap) {
+        String codeGLNObligatoireStr = professionMap.get("CodeGLNObligatoire");
+        return new OuiNonConverter().transform(codeGLNObligatoireStr);
+    }
+
+    private CategorieProfession parseCategorieProfession(Map<String, String> professionMap) {
+        String categorieStr = professionMap.get("Categorie");
+        return CategorieProfession.valueOf(categorieStr);
+    }
+    
 }
