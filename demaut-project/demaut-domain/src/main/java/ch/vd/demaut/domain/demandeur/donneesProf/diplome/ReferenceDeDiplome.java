21fe4f986bdaf849e78e323756e3fadc53c40ac8
Fabien Benichou <fabien.benichou@vd.ch>
2015-11-06 19:43:13 +0100

    Désactive tests services sur Diplome car ne servent a rien
    Former-commit-id: 952c4bcff65db83cfcd67a36dd5ee9963d81ed67

--- demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/donneesProf/diplome/ReferenceDeDiplome.java
+++ demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandeur/donneesProf/diplome/ReferenceDeDiplome.java
@@ -3,6 +3,7 @@ package ch.vd.demaut.domain.demandeur.donneesProf.diplome;
 import ch.vd.demaut.commons.annotations.ValueObject;
 import ch.vd.demaut.commons.vo.BaseValueObject;
 
+// TODO: Supprimer cette classe qui n'a aucun sens
 @ValueObject
 public class ReferenceDeDiplome extends BaseValueObject {
 
