e4f1252ed02a06e4046fd96de5d5d250b8dfeefc
fabien7474 <fabien7474@gmail.com>
2015-11-08 01:32:41 +0100

    Ajout javadoc sur StatutDemande
    Former-commit-id: c7828ef15d5a2ac08845fb07140a1841e142a7e4

--- demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandes/autorisation/StatutDemandeAutorisation.java
+++ demaut-project/demaut-domain/src/main/java/ch/vd/demaut/domain/demandes/autorisation/StatutDemandeAutorisation.java
@@ -1,5 +1,9 @@
 package ch.vd.demaut.domain.demandes.autorisation;
 
+/**
+ * Statut d'une {@link DemandeAutorisation}
+ *
+ */
 public enum StatutDemandeAutorisation {
     Brouillon,
     Soumise,
