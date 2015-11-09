99b041532b74208708d47b8fd0ea4ad3ecdeae32
Fabien Benichou <fabien.benichou@vd.ch>
2015-11-09 21:17:28 +0100

    remove warning
    Former-commit-id: b8ce6fc561796e6bb3ee000dec9aa9079c3595e9

--- demaut-project/demaut-rest/src/main/java/ch/vd/demaut/rest/json/serializer/DateDeNaissanceJsonSerializer.java
+++ demaut-project/demaut-rest/src/main/java/ch/vd/demaut/rest/json/serializer/DateDeNaissanceJsonSerializer.java
@@ -3,7 +3,6 @@ package ch.vd.demaut.rest.json.serializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 
 import ch.vd.demaut.domain.demandeur.donneesPerso.DateDeNaissance;
-import ch.vd.demaut.domain.demandeur.donneesProf.activites.envisagee.DatePrevueDebut;
 
 /**
  * {@link JsonSerializer} pour le {@link DateDeNaissance}
